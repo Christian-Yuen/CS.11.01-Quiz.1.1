3e1266062f277270109eb0f331f788069bd8d551
christian <christiany2026@student.cis.edu.hk>
2023-09-13 09:23:14 +0800

    I did very badly

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
+        String firstName;
+        String favFood;
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Enter your age");
+        int age = scanner.nextInt();
+        System.out.println("Enter your name");
+        firstName = scanner.next();
+        System.out.println("Enter your favourite food");
+        favFood = scanner.next();
+        System.out.println("First name:" + firstName + "\nAge:" + age + "\nFavourite Food:"+ favFood);
     }
 
 }
